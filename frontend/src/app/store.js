e4670f2b79490365ced0bef7531910a3f5b4991b
kkoul13 <kashyapkoul2@gmail.com>
2022-04-08 19:17:02 +0530

    Hooking formdata to Redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -4,7 +4,7 @@ import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
-    auth:authReducer
+    auth:authReducer       // tree name in chrome
   },
 });
 
